59c79971095ef2e44f843e2d6f4abd409dcf8c57
Vraj Parikh <vrajparikh169@gmail.com>
2024-09-05 21:59:52 +0530

    Arrays

--- 02_core/myArray.ts
+++ 02_core/myArray.ts
@@ -9,6 +9,43 @@ const superVillains: string[][] = [
     ["Joker", "Bane"]
 ]; // 2D array
 
+type Users = {
+    name: string;
+    age: number;
+    email: string;
+};
+type Admin = {
+    userName: string;
+    age: number;
+    email: string;
+};
+
+let vraj: Users | Admin = {
+    name: "Vrij",
+    age: 23,
+    email: "v@v.com"
+}
+
+vraj = {
+    userName: "Vraj",
+    age: 23,
+    email: "vraj@Admin.com"
+}
+
+function id (id: string|number){
+    // id + 2;
+    // id.toUpperCase();
+    if(typeof id === "string"){
+        const name = id.toUpperCase();
+        return name;
+    }
+}
+console.log(id("vraj"));
+
+let seatAllotment: "aisle" | "middel" | "window";
+
+seatAllotment = "aisle";
+// seatAllotment = "crew";
 
 
 
