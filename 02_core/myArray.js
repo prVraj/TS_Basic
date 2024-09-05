7eeade5e660e4b230a12412c53d46895bf86f796
Vraj Parikh <vrajparikh169@gmail.com>
2024-09-05 15:58:37 +0530

    arrays

---
+++ 02_core/myArray.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// const superHeroes: string[] = [];
+var superHeroes = [];
+superHeroes.push("Iron-man");
+superHeroes.push("23");
+var superVillains = [
+    ["Lex Luthor", "Brainiac"],
+    ["Joker", "Bane"]
+]; // 2D array
---
+++ 02_core/myArray.ts
@@ -0,0 +1,17 @@
+// const superHeroes: string[] = [];
+const superHeroes: Array<string> = [];
+
+superHeroes.push("Iron-man");
+superHeroes.push("23");
+
+const superVillains: string[][] = [
+    ["Lex Luthor", "Brainiac"],
+    ["Joker", "Bane"]
+]; // 2D array
+
+
+
+
+
+
+export{};
