f177cc5348440639015bcadf9006286896b9cd07
Vraj Parikh <vrajparikh169@gmail.com>
2024-09-05 12:26:00 +0530

    first commit

---
+++ 01_intro/intro.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var user = {
+    name: "Vraj",
+    age: 22,
+};
+var greet = "Hello buddy!!";
+console.log(greet);
+console.log("Vraj");
+console.log(user.name);
---
+++ 01_intro/intro.ts
@@ -0,0 +1,14 @@
+let greet: string = "Hello buddy!!";
+
+console.log(greet.toUpperCase());
+console.log("Vraj");
+
+// Assuming 'user' is defined somewhere else in your code
+// console.log(user.name);
+
+let num: number = 10;
+
+num = 20; // Assigning a number instead of a string
+console.log(num);
+
+export {};
